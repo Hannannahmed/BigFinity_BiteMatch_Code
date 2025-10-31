014e0475bcf9c026b2ee645a51f9c7243d55e1bb
Hannannahmed <133767980+Hannannahmed@users.noreply.github.com>
2025-10-31 08:49:47 +0000

    Update:packagesss update

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+   base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
-    minify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
 });
